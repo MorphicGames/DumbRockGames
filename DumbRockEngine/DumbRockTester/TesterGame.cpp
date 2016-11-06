bab2757b8db98a9035a4587256e6dc8535b3376b
Robert Sponagle <morphic.gaming.net@gmail.com>
2016-11-06 17:42:19 -0500

    Revert "Revert "Revision - 11/06/16""
    This reverts commit 1e45ae653ca3d7b361a500866e24b426a0971c3e.

---
+++ DumbRockEngine/DumbRockTester/TesterGame.cpp
@@ -0,0 +1,55 @@
+#include "TesterGame.h"
+
+namespace DRE
+{
+	TesterGame::TesterGame()
+		: GameEngine()
+	{
+
+	}
+
+	TesterGame::~TesterGame()
+	{
+
+	}
+
+	void TesterGame::StartGame()
+	{
+
+	}
+
+	void TesterGame::OnStart()
+	{
+
+	}
+
+	void TesterGame::OnEnd()
+	{
+
+	}
+
+	void TesterGame::Update()
+	{
+
+	}
+
+	void TesterGame::LateUpdate()
+	{
+
+	}
+
+	void TesterGame::PreRender()
+	{
+
+	}
+
+	void TesterGame::Render()
+	{
+
+	}
+
+	void TesterGame::PostRender()
+	{
+
+	}
+}
