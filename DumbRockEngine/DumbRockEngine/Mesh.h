8da9ffaba11a3fddb8f59f4b1cedae5a4a7a2698
Robert Sponagle <morphic.gaming.net@gmail.com>
2016-11-07 10:49:41 -0500

    Added to Build 11/07/16
    + Added Mesh Skeleton Class
    ~ Mesh.h Needs Revision

---
+++ DumbRockEngine/DumbRockEngine/Mesh.h
@@ -0,0 +1,42 @@
+#ifndef _MESH_H
+#define _MESH_H
+
+#pragma once
+
+class Color;
+
+namespace DRE;
+{
+	class Mesh
+	{
+	public:
+		Mesh();
+		Mesh(Vec3* vertices[], Vec3* uvs[], Vec3* colours[], Vec2* normals[]);
+
+	private:
+		//All data stored within a Mesh
+		Vec3* vertices[];
+		Vec3* uvs[];
+		Color* colours[];
+		Vec2* normals[];
+
+		//Get/Set for Vertices
+		void setVertices(Vec3* vertices[]);
+		Vec3* getVertices();
+
+		//Get/Set for UVs
+		void setUVs(Vec3* uvs[]);
+		Vec3* getUVs();
+
+		//Get/Set for Colours
+		void setColours(Vec3* colours[]);
+		Vec3* getColours();
+
+		//Get/Set for Normals
+		void setNormals(Vec2* normals[]);
+		Vec2* getNormals();
+	};
+}
+
+#endif // !MESH_H
+
