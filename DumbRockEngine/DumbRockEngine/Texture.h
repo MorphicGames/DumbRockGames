ae894cb8156ead7a11ec12fefb8b9cb6bfbdf2fd
Robert Sponagle <morphic.gaming.net@gmail.com>
2016-11-14 20:21:06 -0500

    Build - 11/14/16
    + Added SOIL
    
    ~ Edited Shader
    ~ Edited Texture

--- DumbRockEngine/DumbRockEngine/Texture.h
+++ DumbRockEngine/DumbRockEngine/Texture.h
@@ -1,29 +1,34 @@
 #ifndef _TEXTURE_H
 #define _TEXTURE_H
+
 #pragma once
 
-#include <string>
+#include "OpenGL\glew-1.13.0\include\GL\glew.h"
+#include "OpenGL\Simple OpenGL Image Library\src\SOIL.h"
 
 namespace DRE
 {
 	class Texture
 	{
-	public:
-		Texture() {}
-		// The name of the texture
-		GLuint	texName;
-		// The name of the resource for the texture
-		std::string	resourceName;
-		//Image height
-		int	height;
-		//image width
-		int	width;
+	private:
+		GLuint m_Texture;
+		unsigned char* m_Image;
+		int m_Width, m_Height;
 
+	public:
+		Texture(const char* filename, int width, int height)
+			: m_Width(width)
+			, m_Height(height)
+		{
+			glGenTextures(1, &m_Texture);
+			glBindTexture(GL_TEXTURE_2D, m_Texture);
+				m_Image = SOIL_load_image(filename, &m_Width, &m_Height, 0, SOIL_LOAD_RGB);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_Width, m_Height, 0, GL_RGB, GL_UNSIGNED_BYTE, m_Image);
+				glGenerateMipmap(GL_TEXTURE_2D);
+				SOIL_free_image_data(m_Image);
+			glBindTexture(GL_TEXTURE_2D, 0);
+		}
 	};
 }
 
-
-
-#endif // !_TEXTURE_H
-
-
+#endif 
