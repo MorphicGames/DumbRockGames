e294690bf1914a11e2c4f47f14d390f0bafd52b1
Robert Sponagle <morphic.gaming.net@gmail.com>
2016-11-21 16:48:47 -0500

    Rebuild - 11/21/16
    + Added Model Loading

--- DumbRockEngine/DumbRockEngine/Model.h
+++ DumbRockEngine/DumbRockEngine/Model.h
@@ -4,12 +4,14 @@
 #pragma once
 
 #include <vector>
-//#include <assimp/Importer.h>
-//#include <assimp/scene.h>
-//#include <assimp/postprocess.h>
 
 #include "Mesh.h"
 #include "DRE_typedef.h"
+#include "LogManager.h"
+#include "OpenGL\Simple OpenGL Image Library\src\SOIL.h"
+#include "OpenGL\assimp-3.1.1\include\assimp\Importer.hpp"
+#include "OpenGL\assimp-3.1.1\include\assimp\scene.h"
+#include "OpenGL\assimp-3.1.1\include\assimp\postprocess.h"
 
 namespace DRE
 {
@@ -20,22 +22,129 @@ namespace DRE
 		std::string directory;
 
 		void LoadModel(std::string path) {
+			Assimp::Importer import;
+			const aiScene* scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
 
+			if (!scene || scene->mFlags == AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+			{
+				LogManager::LogMessage(LogMessageType::ERROR, import.GetErrorString(), __FILE__, __LINE__);
+				return;
+			}
+			this->directory = path.substr(0, path.find_last_of('/'));
+
+			this->ProcessNode(scene->mRootNode, scene);
 		}
 
-		void ProcessNode() 
+		void ProcessNode(aiNode* node, const aiScene* scene) 
 		{
+			for (GLuint i = 0; i < node->mNumMeshes; i++)
+			{
+				aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+				this->meshes.push_back(this->ProcessMesh(mesh, scene));
+			}
 
+			for (GLuint i = 0; i < node->mNumChildren; i++)
+			{
+				this->ProcessNode(node->mChildren[i], scene);
+			}
 		}
 
-		Mesh ProcessMesh() 
+		Mesh ProcessMesh(aiMesh* mesh, const aiScene* scene) 
 		{
+			std::vector<Vertex> vertices;
+			std::vector<GLuint> indices;
+			std::vector<Texture> textures;
 
+			for (GLuint i = 0; i < mesh->mNumVertices; i++)
+			{
+				Vertex vertex;
+
+				glm::vec3 vector;
+				vector.x = mesh->mVertices[i].x;
+				vector.y = mesh->mVertices[i].y;
+				vector.z = mesh->mVertices[i].z;
+				vertex.position = vector;
+
+				vector.x = mesh->mNormals[i].x;
+				vector.y = mesh->mNormals[i].y;
+				vector.z = mesh->mNormals[i].z;
+				vertex.normal = vector;
+
+				if (mesh->mTextureCoords[0])
+				{
+					glm::vec2 vec;
+					vec.x = mesh->mTextureCoords[0][i].x;
+					vec.y = mesh->mTextureCoords[0][i].y;
+					vertex.textureCoords = vec;
+				}
+				else
+				{
+					vertex.textureCoords = glm::vec2(0.0f, 0.0f);
+				}
+
+				vertices.push_back(vertex);
+			}
+
+			for (GLuint i = 0; i < mesh->mNumFaces; i++)
+			{
+				aiFace face = mesh->mFaces[i];
+				for (GLuint j = 0; i < face.mNumIndices; j++)
+				{
+					indices.push_back(face.mIndices[j]);
+				}
+			}
+
+			if (mesh->mMaterialIndex >= 0)
+			{
+				aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+				std::vector<Texture> diffuseMaps = this->LoadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
+				textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+				std::vector<Texture> specularMaps = this->LoadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
+				textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+			}
+
+			return Mesh(vertices, indices, textures);
 		}
 
-		std::vector<Texture> LoadMaterialTextures()
+		std::vector<Texture> LoadMaterialTextures(aiMaterial* mat, aiTextureType type, std::string typeName)
 		{
+			std::vector<Texture> textures;
+			for (GLuint i = 0; i < mat->GetTextureCount(type); i++)
+			{
+				aiString str;
+				mat->GetTexture(type, i, &str);
+				Texture texture;
+				texture.id = TextureFromFile(str.C_Str(), this->directory);
+				texture.type = typeName;
+				texture.path = str;
+				textures.push_back(texture);
+			}
 
+			return textures;
+		}
+
+		GLint TextureFromFile(const char* path, std::string directory)
+		{
+			std::string filename = std::string(path);
+			filename = directory + '/' + filename;
+			GLuint textureID;
+			glGenTextures(1, &textureID);
+			int width, height;
+			unsigned char* image = SOIL_load_image(filename.c_str(), &width, &height, 0, SOIL_LOAD_RGB);
+
+			glBindTexture(GL_TEXTURE_2D, textureID);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+			glGenerateMipmap(GL_TEXTURE_2D);
+
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+			glBindTexture(GL_TEXTURE_2D, 0);
+			SOIL_free_image_data(image);
+
+			return textureID;
 		}
 
 	public:
--- DumbRockEngine/DumbRockEngine/Texture.h
+++ DumbRockEngine/DumbRockEngine/Texture.h
@@ -3,31 +3,16 @@
 
 #pragma once
 
+#include <string>
 #include "OpenGL\glew-1.13.0\include\GL\glew.h"
-#include "OpenGL\Simple OpenGL Image Library\src\SOIL.h"
+#include "OpenGL\assimp-3.1.1\include\assimp\Importer.hpp"
 
 namespace DRE
 {
-	class Texture
-	{
-	private:
-		GLuint m_Texture;
-		unsigned char* m_Image;
-		int m_Width, m_Height;
-
-	public:
-		Texture(const char* filename, int width, int height)
-			: m_Width(width)
-			, m_Height(height)
-		{
-			glGenTextures(1, &m_Texture);
-			glBindTexture(GL_TEXTURE_2D, m_Texture);
-				m_Image = SOIL_load_image(filename, &m_Width, &m_Height, 0, SOIL_LOAD_RGB);
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, m_Width, m_Height, 0, GL_RGB, GL_UNSIGNED_BYTE, m_Image);
-				glGenerateMipmap(GL_TEXTURE_2D);
-				SOIL_free_image_data(m_Image);
-			glBindTexture(GL_TEXTURE_2D, 0);
-		}
+	struct Texture {
+		GLuint id;
+		std::string type;
+		aiString path;
 	};
 }
 
